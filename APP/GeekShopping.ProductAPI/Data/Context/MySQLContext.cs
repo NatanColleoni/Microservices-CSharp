8cc722b73fcd632a16325c3fbebc8e3b4157a1d0
Natan Colleoni Fonseca <natancolleonifonseca@gmail.com>
2023-07-05 12:15:45 -0300

    Criação do projeto front sem grandes implementações, criação do microserviço de produtos com entidade de dominio e migrations

---
+++ APP/GeekShopping.ProductAPI/Data/Context/MySQLContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.Product.API.Data.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext() { }
+
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        public DbSet<Domain.Product> Products { get; set; }
+    }
+}
---
+++ APP/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,36 @@
+using GeekShopping.Product.API.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+var connection = builder.Configuration.GetConnectionString("MySQLConnectionString");
+builder.Services.AddDbContext<MySQLContext>(options =>
+{
+    options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 33)));
+});
+
+//Swagger
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping - Product API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
