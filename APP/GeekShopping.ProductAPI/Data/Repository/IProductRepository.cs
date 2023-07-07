31c9e0ce5e2c32758fad7dea798e087594eb1036
Natan Fonseca <TR039271@MinhaTI.com.br>
2023-07-07 12:44:42 -0300

    Criação de interface do repositório de produtos; Remoção do AutoMapper; Criação de operadores explicitos de produto;

---
+++ APP/GeekShopping.ProductAPI/Data/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.Product.API.Data.ValueObjects;
+
+namespace GeekShopping.Product.API.Data.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(int id);
+        Task<ProductVO> Create(ProductVO product);
+        Task<ProductVO> Update(ProductVO product);
+        Task<ProductVO> Delete(int id);
+    }
+}
---
+++ APP/GeekShopping.ProductAPI/Data/ValueObjects/ProductVO.cs
@@ -0,0 +1,38 @@
+﻿namespace GeekShopping.Product.API.Data.ValueObjects
+{
+    public class ProductVO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+
+        public static explicit operator ProductVO(Domain.Product entity)
+        {
+            return new ProductVO()
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Price = entity.Price,
+                Description = entity.Description,
+                CategoryName = entity.CategoryName,
+                ImageUrl = entity.ImageUrl
+            };
+        }
+
+        public static explicit operator Domain.Product(ProductVO vo)
+        {
+            return new Domain.Product()
+            {
+                Id = vo.Id,
+                Name = vo.Name,
+                Price = vo.Price,
+                Description = vo.Description,
+                CategoryName = vo.CategoryName,
+                ImageUrl = vo.ImageUrl
+            };
+        }
+    }
+}
