3c8ff4e7db06a8a8ed08d655bcb68fbd5259f735
Natan Colleoni Fonseca <natancolleonifonseca@gmail.com>
2023-07-12 19:13:35 -0300

    Tela de criação de produtos

--- APP/GeekShopping.Web/Controllers/ProductController.cs
+++ APP/GeekShopping.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using GeekShopping.Web.Services.IServices.IProductService;
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices.IProductService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.Web.Controllers
@@ -16,6 +17,24 @@ namespace GeekShopping.Web.Controllers
         {
             var products = await _service.FindAll();
             return View(products);
+        }
+
+        public async Task<IActionResult> CreateView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductModel product)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _service.Create(product);
+                
+                if (response is not null) return RedirectToAction(nameof(response));
+            }
+
+            return View();
         }
     }
 }
